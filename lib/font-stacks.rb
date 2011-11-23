53273546e261a7db05b885e9f5c2d4d86af0e9c5
Adam Stacoviak <adam@stacoviak.com>
2011-11-23 01:39:31 -0600

    Updated to case and space based naming and added support for monospace

--- lib/font-stacks.rb
+++ lib/font-stacks.rb
@@ -3,7 +3,7 @@ Compass::Frameworks.register("font-stacks", :path => "#{File.dirname(__FILE__)}/
 
 module FontStacks
   
-  VERSION = "0.0.1"
-  DATE = "2011-09-15"
+  VERSION = "0.1.0"
+  DATE = "2011-11-22"
 
 end
