1858064743323abc6f9e09bc5bbf10ab057c2d7a
Adam Stacoviak <adam@stacoviak.com>
2011-11-25 04:52:41 -0600

    Updated the changelog and bumped to 0.1.4

--- lib/font-stacks.rb
+++ lib/font-stacks.rb
@@ -3,7 +3,7 @@ Compass::Frameworks.register("font-stacks", :path => "#{File.dirname(__FILE__)}/
 
 module FontStacks
   
-  VERSION = "0.1.3"
-  DATE = "2011-11-23"
+  VERSION = "0.1.4"
+  DATE = "2011-11-25"
 
 end
